0ed2fb5b349e144a704e89e4a233871c4ddbdf8b
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2024-04-04 17:03:14 +0100

    update allowed_hosts setting

--- backend/hortspurt/hortspurt/settings.py
+++ backend/hortspurt/hortspurt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'hortspurt.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default=os.getenv("DB_URL"),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
 }
