e17fdd74dc73a67c358b123f2bef3fd5b243e1e8
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2024-04-10 16:04:13 +0100

    update settings.py

--- backend/hortspurt/hortspurt/settings.py
+++ backend/hortspurt/hortspurt/settings.py
@@ -129,10 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DEV = os.getenv("DEV")
+if DEV == "True":
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # if not DEBUG:
 #     #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
